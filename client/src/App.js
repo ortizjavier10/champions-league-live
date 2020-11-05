57fdb778d40374e2612bbd2284cbacb3051a0b01
JAVI <ortiz.javier10@gmail.com>
2020-11-04 20:52:54 -0500

    folder structure

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Champions League Live 
+    </div>
+  );
+}
+
+export default App;
