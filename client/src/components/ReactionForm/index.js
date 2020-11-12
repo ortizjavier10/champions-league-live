4f11c4d13cbc1c9fec8e7300702e00738cfb7d1a
JAVI <ortiz.javier10@gmail.com>
2020-11-12 18:24:23 -0500

    bug fixes

--- client/src/components/ReactionForm/index.js
+++ client/src/components/ReactionForm/index.js
@@ -41,7 +41,7 @@ const ReactionForm = ({ thoughtId }) => {
         onSubmit={handleFormSubmit}
       >
         <textarea
-          placeholder="Leave a reaction to this thought..."
+          placeholder="Leave a reply to this comment..."
           value={reactionBody}
           className="form-input col-12 col-md-9"
           onChange={handleChange}
